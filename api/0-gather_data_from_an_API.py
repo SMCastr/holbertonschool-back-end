e40ec6dc6b3cef84cd0d577f01b8404ac68c20b9
SMCastr <mariesweetycastro@gmail.com>
2023-12-19 16:14:16 -0400

    update correctly form & output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Python script that, using a REST API, for a given employee ID,
 returns information about his/her TODO list progress.
@@ -15,26 +15,38 @@ if __name__ == "__main__":
 
     employee_id = argv[1]
 
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    # Fetch user information
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
 
-    response = requests.get(url)
+    if user_response.status_code == 200:
+        employee_name = user_data.get("username", "")
+        if not employee_name:
+            print("Error: Unable to fetch employee name.")
+            exit()
 
-    if response.status_code == 200:
+        # Fetch TODO list for the employee
+        todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+        todo_response = requests.get(todo_url)
 
-        todos = response.json()
+        if todo_response.status_code == 200:
 
-        if todos:
-            employee_name = todos[0]["username"]
+            todos = todo_response.json()
 
-            total_tasks = len(todos)
-            completed_tasks = sum(1 for todo in todos if todo["completed"])
+            if todos:
+                total_tasks = len(todos)
+                completed_tasks = sum(1 for todo in todos if todo["completed"])
 
-            print("Employee {} is done with tasks ({}/{}):".format(
-                employee_name, completed_tasks, total_tasks))
+                print("Employee {} is done with tasks ({}/{}):".format(
+                    employee_name, completed_tasks, total_tasks))
 
-            for todo in todos:
-                print("\t {}".format(todo["title"]))
+                for idx, todo in enumerate(todos, start=1):
+                    task_status = "OK" if todo["completed"] else "NOT OK"
+                    print("Task {} in output: {}".format(idx, task_status))
+            else:
+                print("Employee {} has no tasks.".format(employee_name))
         else:
-            print("Employee with ID {} not found.".format(employee_id))
+            print("Error: Unable to fetch TODO list data from API")
     else:
-        print("Error: Unable to fetch data from API")
+        print("Error: Unable to fetch user data from API")
