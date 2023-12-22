f27fefc05fd024040dc96925d405889b9588af72
SMCastr <mariesweetycastro@gmail.com>
2023-12-22 15:14:13 -0400

    or

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,7 +8,8 @@ and exports to JSON.
 
 Format must be: { "USER_ID": [{"task": "TASK_TITLE",
             "completed": TASK_COMPLETED_STATUS,
-            "username": USERNAME}, ...]}
+            "username": USERNAME},
+]}
 """
 
 import json
@@ -30,41 +31,28 @@ def export_to_json(employee_id: List[Dict[str, str]]) -> None:
         employee_id = employee["id"]
         employee_name = employee["name"]
 
-        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+        api_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+        api_url2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
-        response = requests.get(url)
+        response = requests.get(api_url).json()
+        EMPLOYEE_NAME = response.get('username')
 
-        if response.status_code == 200:
-            todos = response.json()
+        response = requests.get(api_url2).json()
 
-            json_filename = "{}.json".format(employee_id)
+        json_filename = "{}.json".format(employee_id)
 
-            tasks_list = []
-            for todo in todos:
-                task_dict = {
-                    "task": todo["title"],
-                    "completed": todo["completed"],
-                    "username": employee_name
-                }
-                tasks_list.append(task_dict)
+        tasks_list = []
+        for info in response:
+            task_dict = {
+                "task": info.get('title'),
+                "completed": info.get('completed'),
+                "username": EMPLOYEE_NAME
+            }
+            tasks_list.append(task_dict)
 
-            data_dict = {str(employee_id): tasks_list}
+        data_dict = {str(employee_id): tasks_list}
 
-            with open(json_filename, "w") as jsonfile:
-                json.dump(data_dict, jsonfile)
+        with open(json_filename, "w", encoding="utf-8") as jsonfile:
+            json.dump(data_dict, jsonfile)
 
-            print("JSON file '{}' created successfully.".format(json_filename))
-        else:
-            print("Error: Unable to fetch data from API")
-
-# User ID's value is a list of dicts
-employee_id: List[Dict[str, str]] = [{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]
-
-# All tasks found in list of dicts
-tasks = [{"task": "Task 1", "completed": True}, {"task": "Task 2", "completed": False}]
-
-# Ensure employee_id is a list of dicts
-if isinstance(employee_id, dict):
-    employee_id = [employee_id]
-
-export_to_json(employee_id)
+        print("JSON file '{}' created successfully.".format(json_filename))
