9678b76ee7edd9576f1d054fb12f6b65843bcf2a
SMCastr <mariesweetycastro@gmail.com>
2023-12-22 12:00:57 -0400

    json Update & add functions

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,7 +6,7 @@ for tasks owned by a given employee.
 Requirements: Records all tasks that are owned by this employee
 and exports to JSON.
 
-Format must be: { "USER_ID": [ {"task": "TASK_TITLE",
+Format must be: { "USER_ID": [{"task": "TASK_TITLE",
                 "completed": TASK_COMPLETED_STATUS,
                 "username": USERNAME},
                 {"task": "TASK_TITLE",
@@ -21,39 +21,42 @@ from collections import OrderedDict
 from typing import List, Dict
 
 
-def export_to_json(employee_id: str) -> None:
+def export_to_json(employee_id: List[Dict[str, str]]) -> None:
     """
     Exports tasks owned by a given employee to JSON format.
-    Args: employee_id (str): The ID of the employee.
-    Returns:None
+    Args: employee_id (List[Dict[str, str]]): The list of employee IDs and names.
+    Returns: None
     """
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    for employee in employee_id:
+        employee_id = employee["id"]
+        employee_name = employee["name"]
 
-    response = requests.get(url)
+        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
 
-    if response.status_code == 200:
-        todos = response.json()
-        username = todos[0]["username"]
+        response = requests.get(url)
 
-        json_filename = "{}.json".format(employee_id)
+        if response.status_code == 200:
+            todos = response.json()
 
-        tasks_list = []
-        for todo in todos:
-            task_dict = {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": username
-            }
-            tasks_list.append(task_dict)
+            json_filename = "{}.json".format(employee_id)
 
-        data_dict = {employee_id: tasks_list}
+            tasks_list = []
+            for todo in todos:
+                task_dict = {
+                    "task": todo["title"],
+                    "completed": todo["completed"],
+                    "username": employee_name
+                }
+                tasks_list.append(task_dict)
 
-        with open(json_filename, "w") as jsonfile:
-            json.dump(data_dict, jsonfile)
+            data_dict = {str(employee_id): tasks_list}
 
-        print("JSON file '{}' created successfully.".format(json_filename))
-    else:
-        print("Error: Unable to fetch data from API")
+            with open(json_filename, "w") as jsonfile:
+                json.dump(data_dict, jsonfile)
+
+            print("JSON file '{}' created successfully.".format(json_filename))
+        else:
+            print("Error: Unable to fetch data from API")
 
 # User ID's value is a list of dicts
 employee_id = [{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]
